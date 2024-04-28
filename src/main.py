8a549980ffde0d43752b71cf59a065d499c44cea
Rajtilak Bhattacharjee <rajtilak.blog@gmail.com>
2024-04-28 22:25:25 +0530

    Modify main.py to introduce Typer

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 import os
+from typing import Annotated
+from typing import Optional
 
+import typer
 from dotenv import load_dotenv
 from utils.blogger import Blogger
 from utils.downloader import Downloader
@@ -15,15 +18,38 @@ from utils.transcriber import Transcriber
 load_dotenv()
 api_key = os.getenv("GEMMA_API_KEY")
 
+__version__ = "2.0.0"
 
-if __name__ == "__main__":
-    url = input("Enter YouTube URL: ")
-    path_to_tesseract = input(
-        "Please enter the path where Tesseract is installed in your system: ",
-    )
+
+def version_callback(value: bool):
+    if value:
+        print(f"Simone CLI Version: {__version__}")
+        raise typer.Exit()
+
+
+def main(
+    url: Annotated[
+        str,
+        typer.Option(
+            help="URL of the YouTube video. Should be within quotes.",
+            rich_help_panel="Customization and Utils",
+        ),
+    ],
+    path: Annotated[
+        str,
+        typer.Option(
+            help="Path where Tesseract is installed in your system. Should be within quotes.",
+            rich_help_panel="Customization and Utils",
+        ),
+    ],
+    version: Annotated[
+        Optional[bool],
+        typer.Option("--version", callback=version_callback),
+    ] = None,
+):
 
     print("Downloading audio and video...")
-    downloads = Downloader(url)
+    downloads = Downloader(f"{url}")
     downloads.audio()
     downloads.video()
 
@@ -43,7 +69,7 @@ if __name__ == "__main__":
     frames = Framer("video.mp4")
     frame = frames.get_video_frames()
 
-    scores = Scorer(frame, keyword, path_to_tesseract)
+    scores = Scorer(frame, keyword, f"{path}")
     score = scores.score_frames()
 
     print("Saving screenshots...")
@@ -57,3 +83,7 @@ if __name__ == "__main__":
     os.remove("video.mp4")
 
     print("Blog post and 3 screenshots have been generated!")
+
+
+if __name__ == "__main__":
+    typer.run(main)
